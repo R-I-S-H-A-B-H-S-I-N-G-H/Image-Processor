f47f5503287ea53132a8f4150bf791260984c628
rishabh <rishabhsingh2305@gmail.com>
2024-04-28 21:01:03 +0530

    switched to sharp

--- src/utils/Image.js
+++ src/utils/Image.js
@@ -1,4 +1,6 @@
 const Jimp = require("jimp");
+const sharp = require("sharp");
+const axios = require("axios");
 
 exports.IMAGE_MIME_TYPE = {
 	png: "image/png",
@@ -10,7 +12,7 @@ exports.IMAGE_MIME_TYPE = {
 	webp: "image/webp",
 };
 
-exports.editImage = async (props) => {
+exports.editImageGimp = async (props) => {
 	const { rotate, scale, imgUrl, contrast, brightness, invert, grayscale, resize } = props;
 	const jimpReader = await Jimp.read(imgUrl);
 	console.log(imgUrl);
@@ -32,7 +34,31 @@ exports.editImage = async (props) => {
 	return jimpReader.getBufferAsync(Jimp.AUTO);
 };
 
+exports.editImage = async (props) => {
+	const { rotate, scale, imgUrl, contrast, brightness, grayscale, resize, flip, flop } = props;
+	const inputImg = (await axios({ url: imgUrl, responseType: "arraybuffer" })).data;
+
+	const imgObj = sharp(inputImg);
+
+	if (grayscale) imgObj.grayscale();
+	if (flip) imgObj.flip();
+	if (flop) imgObj.flop();
+
+	if (rotate) imgObj.rotate(parseInt(rotate));
+	if (scale) imgObj.scale(parseInt(scale));
+
+	if (contrast) imgObj.linear(parseInt(contrast), -(128 * parseInt(contrast)) + 128);
+	if (brightness) imgObj.modulate({ brightness: parseInt(brightness) });
+
+	if (resize) {
+		const { width, height } = parseSize(resize);
+		imgObj.resize(width, height);
+	}
+
+	return imgObj.toBuffer();
+};
+
 function parseSize(size = "", separator = "x") {
 	const sizeArr = size.split(separator);
-	return { width: parseInt(sizeArr[0]), height: parseInt(sizeArr[1]) };
+	return { width: parseInt(sizeArr[0]) || null, height: parseInt(sizeArr[1]) || null };
 }
