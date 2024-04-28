a8dd5360f2789edd1b225e23547bfd665114864b
rishabh <rishabhsingh2305@gmail.com>
2024-04-28 15:02:39 +0530

    added support for mime type and diff image formats

---
+++ src/utils/Image.js
@@ -0,0 +1,9 @@
+exports.IMAGE_MIME_TYPE = {
+	png: "image/png",
+	apng: "image/apng",
+	avif: "image/avif",
+	gif: "image/gif",
+	jpeg: "image/jpeg",
+	svg: "image/svg+xml",
+	webp: "image/webp",
+};
